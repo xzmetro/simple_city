7697d1540f14ccfc43ae8f696d31f08f3a54fded
xzmetro <xzmetro@outlook.com>
2026-08-22 13:15:35 +0800

    Deleted 2 File and Modified Mod Entrypoint LOGGER import

--- src/main/java/org/xzmetro/mod/main/XiaozhongMetro.java
+++ src/main/java/org/xzmetro/mod/main/XiaozhongMetro.java
@@ -4,9 +4,8 @@ import org.xzmetro.mod.BugUnfixedException;
 import org.xzmetro.mod.block.ModBlocks;
 import org.xzmetro.mod.sound.ModSounds;
 import org.xzmetro.mod.tab.ModCreativeTabs;
+import org.xzmetro.mod.util.Constants;
 import net.fabricmc.api.ModInitializer;
-
-import static com.mojang.text2speech.Narrator.LOGGER;
 
 public class XiaozhongMetro implements ModInitializer {
 	@Override
@@ -14,7 +13,7 @@ public class XiaozhongMetro implements ModInitializer {
 		ModSounds.initialize();
 		ModBlocks.initialize();
 		ModCreativeTabs.initialize();
-		LOGGER.warn("[Xiaozhong's Metro] 版本存在已知错误！", new BugUnfixedException("方块\"xzmetro:fire_extinguisher_cabinet#opened=true\"模型异常，目前未修复！"));
-		LOGGER.info("[Xiaozhong's Metro] Mod Loaded!");
+		Constants.LOGGER.warn("[Xiaozhong's Metro] 版本存在已知错误！", new BugUnfixedException("方块\"xzmetro:fire_extinguisher_cabinet#opened=true\"模型异常，目前未修复！"));
+		Constants.LOGGER.info("[Xiaozhong's Metro] Mod Loaded!");
 	}
 }
