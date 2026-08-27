8785d8b9ec2d5826ac6cdbe0a1dc75d54fe2a82e
xzmetro <xzmetro@outlook.com>
2026-08-27 12:12:11 +0800

    Modified Mod Metadata -> simple_city

--- src/main/java/org/xzmetro/mod/block/TracksideBaliseBlock.java
+++
@@ -1,9 +0,0 @@
-package org.xzmetro.mod.block;
-
-import net.minecraft.world.level.block.Block;
-
-public class TracksideBaliseBlock extends Block {
-    public TracksideBaliseBlock(Properties properties) {
-        super(properties);
-    }
-}
